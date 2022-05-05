2ee12572d5852e5526c7a2c33b43067823d93a51
starlingvibes <starlingroot@protonmail.com>
2022-05-05 22:26:04 +0100

    added grocery bud and slider project

---
+++ 15-Slider/app.js
@@ -0,0 +1,40 @@
+const slides = document.querySelectorAll('.slide');
+const nextBtn = document.querySelector('.nextBtn');
+const prevBtn = document.querySelector('.prevBtn');
+
+slides.forEach(function (slide, index) {
+  slide.style.left = `${index * 100}%`;
+});
+
+let counter = 0;
+nextBtn.addEventListener('click', function () {
+  counter++;
+  carousel();
+});
+prevBtn.addEventListener('click', function () {
+  counter--;
+  carousel();
+});
+
+function carousel() {
+  //   working with slides
+  if (counter > slides.length - 1) {
+    counter = 0;
+  } else if (counter < 0) {
+    counter = slides.length - 1;
+  }
+  //   if (counter === slides.length - 1) {
+  //     nextBtn.style.display = 'none';
+  //   } else {
+  //     nextBtn.style.display = 'block';
+  //   }
+  //   if (counter <= 0) {
+  //     prevBtn.style.display = 'none';
+  //   } else {
+  //     prevBtn.style.display = 'block';
+  //   }
+  slides.forEach(function (slide) {
+    slide.style.transform = `translateX(-${counter * 100}%)`;
+  });
+}
+// prevBtn.style.display = 'none';
